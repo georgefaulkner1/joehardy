961858120b975793cf17c10ddd38605bee8f6505
George Faulkner <gfaulkner675@gmail.com>
2022-09-22 10:29:08 +0100

    initial

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -143,6 +143,8 @@
             background-image: url("https://i.ibb.co/3v3j5jy/home-mobile-background.jpg");    
             background-repeat: no-repeat;
             background-size: contain;
+            margin-top: 100px;
+            height: calc(100vh - 100px);
         }
     }
 
