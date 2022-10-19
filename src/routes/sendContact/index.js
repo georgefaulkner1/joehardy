be35349c7adac2d81e5c8a36957024132f2fad7f
George Faulkner <gfaulkner675@gmail.com>
2022-10-19 13:21:14 +0100

    intiial

--- src/routes/sendContact/index.js
+++ src/routes/sendContact/index.js
@@ -9,21 +9,24 @@ export async function POST({ request }) {
   if(name.length > 1 && email.length > 1 && message.length > 1){
 
     var transporter = nodemailer.createTransport({
-      service: process.env.emailService,
+      service: "gmail", //process.env.emailService
       auth: {
-        user: process.env.emailAuth,
-        pass: process.env.emailPassword
+        user: "gfaulkner675@gmail.com", //process.env.emailAuth
+        pass: "zusxhjwzexfaqkbj" //process.env.emailPassword
       }
     })
 
     var mailOptions = {
       from: 'Website Questions',
-      to: process.env.emailTo,
+      to: "gfaulkner675@gmail.com", //process.env.emailTo
       subject: 'Website Question',
       html: `Name: <b>${name}</b> <br>Phone: <b>${phone}</b> <br>Email: <b>${email}</b> <br>Message: <b>${message}</b>  `
     }
 
+    console.log("Send")
     transporter.sendMail(mailOptions, function(error, info){
+      console.log("Sending")
+      console.log(info)
       if (error) {
         console.log(error);
         return {
